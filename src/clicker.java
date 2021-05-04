5080e78ec38d5f3a0de15ec39576bccffd51e26e
ChristianBackstrom <christian.backstrom@elev.ga.ntig.se>
2021-05-04 13:40:11 +0200

    feat: clicker bas klar

---
+++ src/clicker.java
@@ -0,0 +1,41 @@
+public class clicker {
+    private int clicks;
+    private int clicksPerSec;
+    private int clicksPerClick;
+
+    public clicker(){
+        clicks = 0;
+        clicksPerSec = 0;
+        clicksPerClick = 1;
+    }
+
+    public void increaseCPC(int i){
+        clicksPerClick += i;
+    }
+
+    public void increaseCPS(int i){
+        clicksPerSec += i;
+    }
+
+    public void increaseClicks(int i){
+        clicks += i;
+    }
+
+    public void inreaseTenCPS(){
+        if (clicks <= 1000){
+            increaseCPS(10);
+        }
+    }
+
+    public int getClicks() {
+        return clicks;
+    }
+
+    public int getClicksPerSec() {
+        return clicksPerSec;
+    }
+
+    public int getClicksPerClick() {
+        return clicksPerClick;
+    }
+}
---
+++ src/controller.java
@@ -0,0 +1,5 @@
+public class controller {
+    public static void main(String[] args) {
+
+    }
+}
