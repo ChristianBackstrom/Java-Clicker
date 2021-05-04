90b7cba0096bb66f3ef30c06edc0fcef3f5f7bc5
ChristianBackstrom <christian.backstrom@elev.ga.ntig.se>
2021-05-04 13:11:16 +0200

    feat: view made with minimalistic design

---
+++ src/view.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class view extends JFrame {
+
+    private JButton plusClicks;
+    private JPanel panel;
+    private JTextField value;
+
+    public view(){
+        panel = new JPanel();
+        value = new JTextField("0 glizzys", 40);
+        plusClicks = new JButton("+1 glizzys");
+
+        panel.add(value);
+        panel.add(plusClicks);
+
+        this.add(panel);
+
+        value.setEditable(false);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000,800);
+        this.setVisible(true);
+    }
+
+    public void updateClicks(int i){
+        value.setText(i + " glizzys");
+    }
+
+    public void updateClickValue(int i){
+        plusClicks.setText("+" + i + " glizzys");
+    }
+}
